a8df5645dd4ccb64533499aca99df1e50c2d13fd
NooFivee <pth.trunghieu@gmail.com>
2026-05-15 13:52:41 +0700

    feat(management): add core singleton + scene utilities
    Add management scripts (Singleton, SceneManagement, CameraController, UIFade, AreaEntrance/AreaExit) under Assets/Scripts/Management.

---
+++ Assets/Scripts/Management/CameraController.cs
@@ -0,0 +1,14 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : Singleton<CameraController>
+{
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+   public void SetPlayerCameraFollow()
+    {
+        cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
+
+    }
+}
